fee3824536c37a21056b09f6ac884c62a8053e6c
ludanova <irynaludanova@gmail.com>
2021-10-24 19:29:51 +0300

    Decomposition and create a custom hook usePosts()

--- src/App.js
+++ src/App.js
@@ -3,31 +3,14 @@ import "./App.css"
 import { PostFilter } from "./components/PostFilter/PostFilter"
 import { PostForm } from "./components/PostForm/PostForm"
 import { PostList } from "./components/PostList/PostList"
+import { usePosts } from "./hooks/usePosts"
 import { MyButton } from "./UI/button/MyButton"
 import { MyModal } from "./UI/modal/MyModal"
 function App() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: "React post", body: "Post about React" },
-    { id: 2, title: "Vue post", body: "Post about Vue" },
-    { id: 3, title: "Angular post", body: "Post about Angular" },
-  ])
-
+  const [posts, setPosts] = useState([])
   const [filter, setFilter] = useState({ sort: "", query: "" })
   const [modal, setModal] = useState(false)
-  const sortedPosts = useMemo(() => {
-    if (filter.sort) {
-      return [...posts].sort((a, b) =>
-        a[filter.sort].localeCompare(b[filter.sort])
-      )
-    }
-    return posts
-  }, [filter.sort, posts])
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter((post) =>
-      post.title.toLowerCase().includes(filter.query)
-    )
-  }, [filter.query, sortedPosts])
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -44,7 +27,6 @@ function App() {
       <MyModal visible={modal} setVisible={setModal}>
         <PostForm create={createPost} />
       </MyModal>
-
       <hr />
       <PostFilter filter={filter} setFilter={setFilter} />
       <PostList
